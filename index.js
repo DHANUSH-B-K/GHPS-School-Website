a5eaa9688d80032a19fec9415e2c3db3282a7c27
DHANUSH B K <135241587+DHANUSH-B-K@users.noreply.github.com>
2023-12-16 18:53:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('admission-form-fields').addEventListener('submit', function(event) {
+    event.preventDefault();
+    document.getElementById('admission-form').id = 'admission-form-submitted';
+  });
+  
+   
+ 
